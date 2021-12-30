5738056403829e46949b76175815dddf378529e9
Ioana Anton <Anton.Co.Ioana@student.utcluj.ro>
2021-12-30 23:53:25 +0200

    Meniu de selectare grup si redirectare la pagina grupului pe pagina de profesor +(de mutat clasele din controler in Models)

---
+++ ProiectIS/Models/DateGrup.cs
@@ -0,0 +1,12 @@
+﻿namespace ProiectIS.Models
+{
+    public class DateGrup
+    {
+        public long id { get; set; }
+        public string nume { get; set; }
+        public string descriere { get; set; }
+
+
+        public DateGrup(List<Object> list) { id = (long)list[0]; nume = (string)list[1]; descriere = (string)list[2]; }
+    }
+}
